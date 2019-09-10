ae1f918e61371dbfe7eca4bb2833865ad572b6ac
MarvelousMoses <825306937@qq.com>
2019-09-10 15:39:44 +0800

    完成3.6.4

--- apitest/views.py
+++ apitest/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
+from  apitest.models import Apitest,Apistep
 from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
@@ -24,15 +26,34 @@ def login(request):
             response = HttpResponseRedirect('/home/')
             return response
         else:
-            return render(request,'login.html', {'error': 'username or password error'})
+            return render(request, 'login.html', {'error': 'username or password error'})
     # else:
     #     context = {}
     #     return render(request, 'login.html', context)
-    return render(request,'login.html')
+    return render(request, 'login.html')
+
 
 def home(request):
     return render(request, "home.html")
 
+
 def logout(request):
     auth.logout(request)
-    return render(request,'login.html')
+    return render(request, 'login.html')
+
+
+# 接口管理
+@login_required
+def apitest_manage(request):
+    apitest_list = Apitest.objects.all() #读取所有流程接口数据
+    username = request.session.get('user', '') # 读取浏览器登录 Session
+    return render(request,"apitest_manage.html",{"user": username,"apitests":apitest_list})
+    # 定义流程接口数据的变量并返回到前端
+
+
+# 接口步骤管理
+@login_required
+def apistep_manage(request):
+    username = request.session.get('user', '')
+    apistep_list = Apistep.objects.all()
+    return render(request, "apistep_manage.html", {"user": username,"apisteps": apistep_list})
